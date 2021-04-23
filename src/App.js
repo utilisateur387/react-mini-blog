8723ded8de6be5d0e7572a6a0fcb81208bd6a751
Miebi Iyeyemi <iyeyemim@gmail.com>
2021-04-23 18:23:02 +0200

    Create navbar component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
+import Navbar from './Navbar';
 
 function App() {
+  const title = "Welcome to the new blog";
+  const likes = 50;
+  const mySite = "http://miebi.space";
+
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
-        <h1>App Component</h1>
+        <h1>{title}</h1>
+        <p>Liked {likes} times!</p>
+
+        <p>{2 + 3}</p>
+        <p>{[1,2,3,4,5].join(" ")}</p>
+        <a href={mySite} target="_blank">My portfolio</a>
       </div>
     </div>
   );
