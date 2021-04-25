6c7049641d8d709a9956eb1a8015e3761e8910ab
Miebi Iyeyemi <iyeyemim@gmail.com>
2021-04-25 22:10:28 +0200

    Create BlogList component

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import BlogList from './BlogList';
 
 const Home = (props) => {
   const [blogs, setBlogs] = useState([
@@ -7,15 +8,14 @@ const Home = (props) => {
     { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
   ]);
 
+  const handleDelete = (id) => {
+    setBlogs((prev) => prev.filter(blog => blog.id !== id));
+  };
+
   return (
     <div className="home">
-      {blogs.map((blog)=> (
-          <div className="blog-preview" key={blog.id}>
-            <h2>{blog.title}</h2>
-            <p>{blog.author}</p>
-          </div>
-        )
-      )}
+      <BlogList blogs={blogs} title="All blogs" handleDelete={handleDelete} />
+      <BlogList blogs={blogs.filter(blog => blog.author === 'mario')} title="Marios's blogs"/>
     </div>
   );
 }
