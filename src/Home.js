79da3b46089649978496f87bc2c49cd4011ec89d
Miebi Iyeyemi <iyeyemim@gmail.com>
2021-05-03 19:44:33 +0200

    Add comments

--- src/Home.js
+++ src/Home.js
@@ -4,20 +4,32 @@ import BlogList from './BlogList';
 const Home = (props) => {
   const [blogs, setBlogs] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     setTimeout(() => {
-      fetch('http://localhost:8000/blogs')
-      .then(response => response.json())
-      .then(data => {
-        setIsLoading(false);
-        setBlogs(data);
-      })
-    }, 400)
+      fetch('http://localhost:8000/blogs') // FETCH DATA FROM FAKE JSON API
+        .then(response => {
+          if (!response.ok) { // THROW AN ERROR IF ERROR IS NOT OK
+            throw Error('Could not fetch the data for that resource')
+          }
+          return response.json() // ELSE, RETURN THE RESPONSE
+        })
+        .then(data => {
+          setIsLoading(false); // ISLOADING IS NOW FALSE
+          setBlogs(data); // SET BLOGS VARIABLE WITH FETCHED DATA
+          setError(null); // THERE IS NO ERROR
+        })
+        .catch(err => {
+          setError(err.message);
+          setIsLoading(false);
+        })
+    }, 400);
   }, []);
 
   return (
     <div className="home">
+      { error && <div>{ error }</div>}
       { isLoading && <div>Loading...</div> }
       { blogs && <BlogList blogs={blogs} title="All blogs" /> }
     </div>
