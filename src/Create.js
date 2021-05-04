1adfb4b4eed95640290c60a4ba672adcf3a561bd
Miebi Iyeyemi <iyeyemim@gmail.com>
2021-05-04 14:17:59 +0200

    Track form input values as state variables

--- src/Create.js
+++ src/Create.js
@@ -1,4 +1,14 @@
+import { useState } from 'react';
+
 const Create = () => {
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+  const [author, setAuthor] = useState('mario');
+
+  const handleTitleChange = (e) => {
+    setTitle(e.target.value);
+  };
+
   return (
       <div className="create">
         <h2>Add a New Blog</h2>
@@ -8,19 +18,33 @@ const Create = () => {
           <input
             type="text"
             required
+            value={title}
+            onChange={handleTitleChange}
           />
+
           <label>Blog body:</label>
           <textarea
+            value={body}
+            onChange={(e) => setBody(e.target.value)}
             required
           >
           </textarea>
+
           <label>Blog author:</label>
-          <select>
+          <select
+            value={author}
+            onChange={(e) => setAuthor(e.target.value)}
+          >
             <option value="mario">mario</option>
             <option value="yoshi">yoshi</option>
           </select>
+
           <button>Add blog</button>
         </form>
+
+        <p>{ title }</p>
+        <p>{ body }</p>
+        <p>{ author }</p>
       </div>
     )
 }
