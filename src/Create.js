491b5f49e3d2f3869863f0d9b08f0aca4141d0b6
Miebi Iyeyemi <iyeyemim@gmail.com>
2021-05-04 14:05:40 +0200

    Add CSS to form

--- src/Create.js
+++ src/Create.js
@@ -2,6 +2,25 @@ const Create = () => {
   return (
       <div className="create">
         <h2>Add a New Blog</h2>
+
+        <form>
+          <label>Blog title:</label>
+          <input
+            type="text"
+            required
+          />
+          <label>Blog body:</label>
+          <textarea
+            required
+          >
+          </textarea>
+          <label>Blog author:</label>
+          <select>
+            <option value="mario">mario</option>
+            <option value="yoshi">yoshi</option>
+          </select>
+          <button>Add blog</button>
+        </form>
       </div>
     )
 }
